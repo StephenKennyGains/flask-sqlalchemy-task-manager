7e0b5fe74434916749c0816471bc11c1aa13edbd
StephenKennyGains <stephenkennygains@gmail.com>
2021-12-06 15:44:13 +0000

    Add Task now displaying to users, R in CRUD

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initializaton
-    var sidenav = document.querySelectorAll('.sidenav');
+document.addEventListener("DOMContentLoaded", function() {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 
     // datepicker initialization
@@ -13,4 +13,8 @@ document.addEventListener('DOMContentLoaded', function() {
     // select initialization
     let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
-  });
+
+    // collapsible initializataion
+    let collapsibles = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(collapsibles);
+});
